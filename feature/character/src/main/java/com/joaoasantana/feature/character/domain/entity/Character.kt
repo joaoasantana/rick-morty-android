a8e9fdff0158530a72ecab64aa8242e5c5b0b37f
joaoasantana <joao.asantana@icloud.com>
2025-05-10 18:57:45 -0300

    feat: update character domain entity

--- feature/character/src/main/java/com/joaoasantana/feature/character/domain/entity/Character.kt
+++ feature/character/src/main/java/com/joaoasantana/feature/character/domain/entity/Character.kt
@@ -2,4 +2,10 @@ package com.joaoasantana.feature.character.domain.entity
 
 data class Character(
     val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val image: String,
 )
