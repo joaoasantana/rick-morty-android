cdddb52101bcb73547f41417452d918b9db5c072
joaoasantana <joao.asantana@icloud.com>
2025-05-10 19:50:10 -0300

    feat: update character storage

--- feature/character/src/main/java/com/joaoasantana/feature/character/data/source/CharacterStorage.kt
+++ feature/character/src/main/java/com/joaoasantana/feature/character/data/source/CharacterStorage.kt
@@ -2,13 +2,14 @@ package com.joaoasantana.feature.character.data.source
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.joaoasantana.feature.character.data.model.CharacterEntity
 
 @Dao
 interface CharacterStorage {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(data: List<CharacterEntity>)
 
     @Query("SELECT * FROM character WHERE id = :id LIMIT 1")
