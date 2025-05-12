94a3b25b08fa58f03a35354b6b2496a2cfedf356
joaoasantana <joao.asantana@icloud.com>
2025-05-12 10:49:28 -0300

    feat: integrate character feature with app

--- app/src/main/java/com/joaoasantana/rickandmorty/core/MainNavHost.kt
+++ app/src/main/java/com/joaoasantana/rickandmorty/core/MainNavHost.kt
@@ -1,22 +1,17 @@
 package com.joaoasantana.rickandmorty.core
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.joaoasantana.feature.character.presentation.ui.list.CharacterListScreen
 
 @Composable
 fun MainNavHost(
     navHostController: NavHostController = rememberNavController(),
 ) {
-    NavHost(navHostController, startDestination = "dummy") {
-        composable("dummy") { DummyScreen() }
+    NavHost(navHostController, startDestination = "character_list") {
+        composable("character_list") { CharacterListScreen() }
     }
-}
-
-@Composable
-fun DummyScreen() {
-    Text(text = "Hello World!!")
 }
